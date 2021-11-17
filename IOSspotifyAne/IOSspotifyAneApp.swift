cfed3b2c7a9b421e6c31188ff42c308f041310b4
AneCapde <ane.capdevila@opendeusto.es>
2021-11-17 09:59:50 +0100

    Initial Commit

---
+++ IOSspotifyAne/IOSspotifyAneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSspotifyAneApp.swift
+//  IOSspotifyAne
+//
+//  Created by Ane Capdevila on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct IOSspotifyAneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
